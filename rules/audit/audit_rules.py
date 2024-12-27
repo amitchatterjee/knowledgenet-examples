b9147871d4e6718b79cc99a3301d68554040851b
Amit Chatterjee <amit.chatterjee@quik-j.com>
2024-12-27 06:12:35 -0500

    Added police report

--- rules/audit/audit_rules.py
+++ rules/audit/audit_rules.py
@@ -9,7 +9,7 @@ from autoins.entities import Adj, Action
 @ruledef
 def no_police_report():
     return Rule(id='no-police-report', repository='autoclaims', ruleset='002-audit',
-        when=Condition(of_type=Adj, matches=lambda ctx,this: not this.claim.police_report 
+        when=Condition(of_type=Adj, matches=lambda ctx,this: not this.police_report 
                        and assign(ctx, adj=this)),
         then=lambda ctx: insert(ctx, Action(str(uuid.uuid4()), ctx.adj.claim.id, 'd', 'no police report', 0.00)))
 
