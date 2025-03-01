262ed925c8a951ecfe396dcaf00fd775d976ee22
Amit Chatterjee <amit.chatterjee@quik-j.com>
2025-03-01 14:51:10 -0500

    Ading unit tests

--- autoins/test/unit/test_validation_rules.py
+++ autoins/test/unit/test_validation_rules.py
@@ -1,4 +1,6 @@
-import pytest
+import json
+
+from autoins.entities import Action
 from framework import execute
 
 def test_sanity():
@@ -6,6 +8,9 @@ def test_sanity():
     assert result is not None
 
 
-def todo_validation_rules():
-    result = execute('rules', ['test/data/validation-rules'], '../target/results')
-    assert result is not None
+def test_validation_rules():
+    result_facts = execute('rules', ['test/data/validation-rules'], '../target/results')
+    assert result_facts is not None
+    for result_fact in result_facts:
+            if type(result_fact) == Action:
+                print(f"\t{result_fact.__class__.__name__,}: {json.dumps(result_fact.to_dict())}")
